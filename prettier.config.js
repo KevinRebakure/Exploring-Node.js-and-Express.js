9469cecdad0575ab16a525c3953234eaa866c242
Rebakure <kevinrebakure@gmail.com>
2024-08-24 17:44:14 +0200

    initialized repo

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  pluginSearchDirs: false,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: ["^@", "^[a-zA-Z0-9-]+", "^[./]"],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "views/**/*.ejs"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
